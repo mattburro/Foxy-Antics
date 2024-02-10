1d71cce7bbaae4c0ff57fbac5465102d1784969b
Matt Burroughs <matt.burro@gmail.com>
2024-02-10 16:39:28 -0500

    Create bullets

---
+++ bullets/bullet_base/bullet_base.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var _direction: Vector2 = Vector2.RIGHT
+var _life_span: float = 20.0
+var _life_time: float = 0.0
+
+func _process(delta):
+	check_life_time(delta)
+	position += _direction * delta
+
+func setup(direction: Vector2, speed: float, life_span: float):
+	_direction = direction.normalized() * speed
+	_life_span = life_span
+
+func check_life_time(delta: float):
+	_life_time += delta
+	if _life_time > _life_span:
+		queue_free()
+
+func on_area_entered(area: Area2D):
+	queue_free()
